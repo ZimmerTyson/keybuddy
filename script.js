c321bcf1fcdbba05ab30fab8b6a588cbd7f5a827
Tyson Zimmer <zimmertyson@gmail.com>
2025-07-24 13:09:32 -0700

    Laying out the general layout of what needs to be built out in the JS doc

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
 let pressedKeys = new Set();
 
+
+// Adding keys to the pressedKeys variable on key press
+
 document.addEventListener('keydown', function(e) {
   pressedKeys.add(e.code);
   console.log('Current Pressed Keys: ', pressedKeys);
+  convertKeysToCombo();
 });
+
+
+// Removing keys to the pressedKeys variable on lifting off of key
 
 document.addEventListener('keyup', function(e) {
   pressedKeys.delete(e.code);
   console.log('Current Pressed keys:', pressedKeys);
 });
+
+// Key Converter
+
+let keyConverter = {
+  'ControlLeft': 'Ctrl',
+  'ControlRight': 'Ctrl',
+  'AltLeft': 'Alt',
+  'KeyT': 'T',
+
+};
+
+
+function convertKeysToCombo() {
+  let humanKeys = [];
+
+  for (let keyCode of pressedKeys) {
+    let humanKey = keyConverter[keyCode];
+
+    humanKeys.push(humanKey);
+  }
+  console.log(humanKeys.join(' + '));
+}
+
+// Text Replacement
+
+
+
+// Data Storage
+
+
+
+
+// Extension Plumbing
