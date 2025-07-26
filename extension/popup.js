dc3461ac9ea52bb6a1ee4a48bc3defb7545060cd
Tyson Zimmer <zimmertyson@gmail.com>
2025-07-26 14:30:50 -0700

    starting to think through the storage inside of chrome for popup.js

---
+++ extension/popup.js
@@ -0,0 +1,10 @@
+// Fetch shortcuts when the extension icon is clicked
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Popup Loaded!");
+});
+
+// Save and store shortcuts when the add button is pressed 
+
+document.getElementById("addShortcut").addEventListener("click", () => {
+
+});
